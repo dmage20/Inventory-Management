db9687c2992a7afbd590c33bc3bc79444c8b8d86
Daniel Mage <dmage20@gmail.com>
2019-03-20 09:28:24 +0700

    working customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
   belongs_to :user
-  belongs_to :order
+  has_many :orders
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_one :customer
+  belongs_to :customer
   has_many :order_products
   has_many :products, through: :order_products
 end
